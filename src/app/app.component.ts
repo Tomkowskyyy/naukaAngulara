2a6f455ef17f7a9738acd4987113a13fa454afce
Tomasz Kowalski <t.kowalski1608@gmail.com>
2023-04-15 01:09:24 +0200

    Adding new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
 
+import { Car } from './car'; 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-project';
+
+  title = 'To jest wywolanie zmiennej title z użyciem PIPE uppercase';
+
+  getText(): string{
+    return 'To jest tekst metody getText()';
+  }
+
+  getPerson(){
+    return{
+      name: 'Tomasz',
+      surname: 'Kowalski',
+      age: 23
+    };
+  }
+
+  getInfo(){
+    console.log("Metoda getInfo() została wywołana");
+    return "Tekst wyświetlony z metody";
+  }
+
+  counter = 0;
+
+  count(){
+    this.counter++;
+  }
+
+  notBinded(){
+    console.log("Ta metoda nie jest bindowana");
+  }
+
+  getDate(): Date {
+    console.log("Oto obecna data: " + Date());
+    return new Date();
+  }
+
+  cars: Car[] = [
+    {name:"M3", 
+    brand:"BMW",
+    price: 400000},
+
+    {name:"A4", 
+    brand:"Audi",
+    price: 160000},
+
+    {name:"Golf", 
+    brand:"VW",
+    price: 70000},
+
+    {name:"Mondeo", 
+    brand:"Ford",
+    price: 120000}
+  ];
+
 }
